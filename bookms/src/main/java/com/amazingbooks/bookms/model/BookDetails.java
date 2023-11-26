69850d6b7c4c9f0c5f16c24718ac9c242ae71c23
Dipak122-debug <dipak628das@gmail.com>
2023-11-26 10:56:15 +0530

    BookMs commit

---
+++ bookms/src/main/java/com/amazingbooks/bookms/model/BookDetails.java
@@ -0,0 +1,64 @@
+package com.amazingbooks.bookms.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int isbn;
+	private String title;
+	private Date publishedDate;
+	private int totalCopies;
+	private int issuedCopies;
+	private String author;
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+	public int getTotalCopies() {
+		return totalCopies;
+	}
+	public void setTotalCopies(int totalCopies) {
+		this.totalCopies = totalCopies;
+	}
+	public int getIssuedCopies() {
+		return issuedCopies;
+	}
+	public void setIssuedCopies(int issuedCopies) {
+		this.issuedCopies = issuedCopies;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+}
